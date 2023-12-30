e45614cbbe5a3d359383a24524e785c2fc780631
WVC20-15 <williamvilchezcruz@gmail.com>
2023-12-29 22:55:18 -0500

    Nuevos cambios, agregando librerías

---
+++ Clase 2 Intro R.R
@@ -0,0 +1,21 @@
+
+#Capítulo 2 - Investigación reproducible
+
+#La investigación reproducible implica que desde los mismos datos y/o el mismo código se generarán los mismos resultados.
+
+#https://www.youtube.com/live/CGvQPlpBcso?si=FW-HGpY4_wFlEFV4
+
+#https://github.com/derek-corcoran-barrios/derek-corcoran-barrios.github.io
+
+library(rmarkdown)
+
+library(stargazer)
+
+library(kableExtra)
+
+library(tidyverse)
+
+library(knitr)
+
+#Github es una suerte de dropbox o google drive pensado para la investigación reproducible, 
+#en donde cada proyecto es un repositorio. 
